e7164e0ecc948463cb0f0d263a581bcf9070050d
Christian McArthur <CBMcArthur2@gmail.com>
2015-07-26 08:23:48 -0500

    Create title_generator.php
    Importing initial version of job title generator script.

---
+++ title_generator.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * File: title_generator.php
+ * Author: Christian McArthur
+ * Date Created: 07/25/2015
+ * Last Modified: 07/26/2015
+ *
+ * This is a quick and dirty PHP-based random title generator.
+ * Titles will consist of several parts as follows:
+ *   >Prefix< >Major< of >Minor Prefix< >Minor<
+ * Title parts are randomly selected from an array for each part.
+*/
+
+
+// Title prefixes
+$prefix = ["Assistant", "Vice", "Lead", "Junior", "Chief", "Technical"];
+// Major titles
+$major = ["President", "Manager", "Director", "Analyst", "Designer", "Supervisor"];
+// Minor prefixes
+$preminor = ["Special", "Public", "Private", "Personal", "Advanced", "Benefits"];
+// Minor titles 
+$minor = ["Projects", "Communication", "Development", "Marketing", "Accounting", "Engineering"];
+
+// Randomly select title part elements
+$pick_prefix = rand(0, count($prefix));
+$pick_major = rand(1, count($major));
+$pick_preminor = rand(0, count($preminor));
+$pick_minor = rand(1, count($minor));
+
+// Display the randomly constructed title
+echo ($pick_prefix == 0 ? "" : $prefix[$pick_prefix-1]." ");
+echo $major[$pick_major-1]." of ";
+echo ($pick_preminor == 0 ? "" : $preminor[$pick_preminor-1]." ");
+echo $minor[$pick_minor-1];
+echo "\n";
+?>
